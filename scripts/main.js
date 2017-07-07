8a54488210c55561efb9a6ad338f955c027e95af
Ing. Pavol Šoučík <soucik@gmail.com>
2017-07-07 16:09:22 +0200

    FIXED: es6->es5: Mobile browsers dosnt support es6

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,36 @@
-class ImageNameResolver{
-
-    constructor(datetime){
+function ImageNameResolver(datetime){ //class definition with constructor
         this._pathToImages = 'Images/Weather/';
         this._suffixes = new Array('.jpg','.png');
         this._datetime = datetime;
         this._hour = this._datetime.getHours();
-        this._partOfDay = this.getPartOfDay(this._hour)
+        this._partOfDay = this.getPartOfDay(this._hour);
     }
-    getPartOfDay(){
+
+   ImageNameResolver.prototype.getPartOfDay = function(){
         switch(true){
-            case this._hour >= 5 && this._hour < 7: //sunrise
+            case this._hour >= 5 && this._hour < 7: //sunrise <5,7)
             {
                 return 'sunrise';               
             }
-            case this._hour >= 18 && this._hour <= 20:
+            case this._hour >= 18 && this._hour <= 20: //sunset <18,20>
             {
                 return 'sunset';
             }
-            case (this._hour > 20 && this._hour < 24 ) || (this._hour >= 0 && this._hour < 5 ):
+            case (this._hour > 20 && this._hour <= 24 ) || (this._hour >= 0 && this._hour < 5 ): // (20,24> U <0,5)
             {
                 return 'night';
             }
-            default:
+            default: // <7,18) U all others
             {
                 return 'day';
             }
         }
     }
-    getFullPath(){
+    ImageNameResolver.prototype.getFullPath = function(){
         return this._pathToImages + this._partOfDay + this._suffixes[0];
     }
-}
-$().ready(function(){
 
+$().ready(function(){
     console.log('Loaded');
     var datetime = new Date();
     var resolver = new ImageNameResolver(datetime);
