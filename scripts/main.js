3687ccfa2ca93e3607264a46f9c26ae9648b1139
Ing. Pavol Šoučík <soucik@gmail.com>
2017-07-20 20:55:28 +0200

    Changed: client implementation websocket -> sockets

--- scripts/main.js
+++ scripts/main.js
@@ -30,148 +30,68 @@ ImageNameResolver.prototype.getFullPath = function () {
     return this._pathToImages + this._partOfDay + this._suffixes[0];
 }
 
-function Chat(chatWindow, chatContent, chatInput, chatStatus) {
+function Chat(server) {
 
     self = this;
-    this._chatWindow = $(chatWindow);
-    this._chatContent = $(chatContent);
-    this._chatInput = $(chatInput);
-    this._chatStatus = $(chatStatus);
-
-    this._myColor = false;
-    this._myName = false;
+    this._server = server;
     this._connection = null;
-
+    this._name = null;
 }
 
-Chat.prototype.InitiateConnection = (function () {
+Chat.prototype.connect = function () {
 
-    var newWebSocket = false;
-    return function () {
-        // if user is running mozilla then use it's built-in WebSocket
-        window.WebSocket = window.WebSocket || window.MozWebSocket;
-
-        // if browser doesn't support WebSocket, just show some notification and exit
-        if (!window.WebSocket) {
-            self._chatContent.html($('<p>', {
-                text: 'Sorry, but your browser doesn\'t '
-                + 'support WebSockets.'
-            }));
-            this._chatInput.hide();
-            $('span').hide();
-            return;
-        }
-        if (!newWebSocket) {
-            // open connection
-            self._connection = new WebSocket('ws://services-2320.rostiapp.cz');
-            newWebSocket = true;
-        }
-    }
-})();
-
-Chat.prototype.AddMessage = function (author, message, color, dt) {
-    this._chatContent.append('<p><span style="color:' + color + '">' + author + '</span> @ ' +
-        + (dt.getHours() < 10 ? '0' + dt.getHours() : dt.getHours()) + ':'
-        + (dt.getMinutes() < 10 ? '0' + dt.getMinutes() : dt.getMinutes())
-        + ': ' + message + '</p>');
+    self._socket = io.connect(this._server);
 }
+Chat.prototype.handlers = function () {
 
-Chat.prototype.InitiateHandlers = function () {
-
-    self._connection.onopen = function () {
-        // first we want users to enter their names
-        // this._chatInput.removeAttr('disabled');
-        // this._chatStatus.text('Choose name:');
-    };
-
-    self._connection.onerror = function (error) {
-        // just in there were some problems with conenction...
-        self._chatContent.html($('<p>', {
-            text: 'Sorry, but there\'s some problem with your '
-            + 'connection or the server is down.'
-        }));
-    };
-
-    // most important part - incoming messages
-    self._connection.onmessage = function (message) {
-        // try to parse JSON message. Because we know that the server always returns
-        // JSON this should work without any problem but we should make sure that
-        // the massage is not chunked or otherwise damaged.
-        try {
-            var json = JSON.parse(message.data);
-        } catch (e) {
-            console.log('This doesn\'t look like a valid JSON: ', message.data);
-            return;
-        }
-
-        // NOTE: if you're not sure about the JSON structure
-        // check the server source code above
-        if (json.type === 'color') { // first response from the server with user's color
-            myColor = json.data;
-            self._chatStatus.text(self._myName + ': ').css('color', myColor);
-            self._chatInput.removeAttr('disabled').focus();
-            // from now user can start sending messages
-        } else if (json.type === 'history') { // entire message history
-            // insert every single message to the chat window
-            for (var i = 0; i < json.data.length; i++) {
-                self.AddMessage(json.data[i].author, json.data[i].text,
-                    json.data[i].color, new Date(json.data[i].time));
-            }
-        } else if (json.type === 'message') { // it's a single message
-            self._chatInput.removeAttr('disabled'); // let the user write another message
-            self.AddMessage(json.data.author, json.data.text,
-                json.data.color, new Date(json.data.time));
-        } else {
-            console.log('Hmm..., I\'ve never seen JSON like this: ', json);
-        }
-    };
-
-    /**
-     * Send mesage when user presses Enter key
-     */
-    self._chatInput.keydown(function (e) {
-        if (e.keyCode === 13) {
-            var msg = $(this).val();
-            if (!msg) {
-                return;
-            }
-            // send the message as an ordinary text
-            self._connection.send(msg);
-            $(this).val('');
-            // disable the input field to make the user wait until server
-            // sends back response
-            self._chatInput.attr('disabled', 'disabled');
-
-            // we know that the first message sent from a user their name
-            if (self._myName === false) {
-                self._mmyName = msg;
-            }
-        }
+    self._socket.on('updatechat', function (username, data) {
+        $('#chatContent').append('<b>' + username + ':</b> ' + data + '<br>');
     });
 }
+Chat.prototype.sendMessage = function (message) {
+    console.log('SEND MESSAGE');
+    self._socket.emit('sendchat', message);
+};
 
-Chat.prototype.OptionalInterval = function () {
-    setInterval(function () {
-        if (self._connection.readyState !== 1) {
-            self._chatStatus.text('Error');
-            self._chatInput.attr('disabled', 'disabled').val('Unable to comminucate '
-                + 'with the WebSocket server.');
-        }
-    }, 3000);
-}
+Chat.prototype.connectUser = function (username) {
+    console.log('SET NAME');
+    self._socket.emit('adduser', username, 'AF313C6D972BB5D01908CB9DA8EB1CB8A64FCCEFF96773BF0BC7275E21079B5D321A264E7B0DB0643C5C5D199FD19A1190EA984A78384FDE3B2BB31902A809B6');
+    self._name = username;
+};
 
 $().ready(function () {
-    console.log('Loaded');
+    
+     $("form input").keypress(function (e) {
+        if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
+            $('#datasend').click();
+            return false;
+        } else {
+            return true;
+        }
+    });
     var datetime = new Date();
     var resolver = new ImageNameResolver(datetime);
     $('body').css({ 'background': 'url(' + resolver.getFullPath() + ')  no-repeat', 'background-size': '100% auto', 'background-color': 'green' });
 
     var questionButton = $('#questionButton');
-    questionButton.one('click',function () {
-        $('#chatContent').empty();
-        var chat = new Chat('#chatWindow', '#chatContent', '#chatInput', '#chatStatus');
-        chat.InitiateConnection();
-        chat.InitiateHandlers();
-        chat.OptionalInterval();
-    }).click(function(){console.log('click other than first')});
+    questionButton.one('click', function () {
+        chat = new Chat('ws://services-2320.rostiapp.cz');
+
+    });
+    $('#datasend').on('click', function () {
+
+        var userInput = $('#chatInput').val();
+        if (!chat._name) {
+            chat.connect();
+            chat.handlers();
+            chat.connectUser(userInput);
+            
+            $('.control-label').text('Správa: ');
+            $('#datasend').val('Poslať');
+        }
+        else {
+            chat.sendMessage(userInput);
+        }
+        $('#chatInput').val('');
+    });
 });
